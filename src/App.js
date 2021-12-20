cb09b18a901f030ef30b6a66a45170a17fb74965
Mohannad Zidan <moh.anad23823@gmail.com>
2021-12-20 16:59:13 +0200

    transformed App component to class instead of a function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import React from "react";
 import './App.css';
 import CodeEditor from './components/CodeEditor';
 import InfoSection from './components/InfoSection';
 
-function App() {
-  return (
-    <div className="App">
-      <InfoSection></InfoSection>
-      <CodeEditor></CodeEditor>
-    </div>
-  );
+
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <InfoSection></InfoSection>
+                <CodeEditor></CodeEditor>
+            </div>
+        );
+    }
 }
 
 export default App;
